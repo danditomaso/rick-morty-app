9932aa4d5f939295b144c1aeb5867b47d2fab13a
Dan Ditomaso <dan.ditomaso@gmail.com>
2024-04-11 16:41:29 -0400

    added vitest testing framework to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+/// <reference types="vitest" />
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	test: {
+		name: "happy-dom",
+		environment: "happy-dom",
+		// setupFiles: ["./setup.happy-dom.ts"],
+	},
+});
